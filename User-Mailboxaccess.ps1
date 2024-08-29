5ff3a556e68dc41f8995eb4c5406bcb6be1a4b68
Maximilian <m.schallenkamp@gmail.com>
2024-08-29 11:30:16 +0200

    Delete User-Mailboxaccess.ps1

--- User-Mailboxaccess.ps1
+++
@@ -1,2 +0,0 @@
-﻿$boxaccess =Get-Mailbox -ResultSize Unlimited | ForEach-Object {Get-MailboxPermission -Identity $_.guid} | Get-MailboxPermission -User "Maximilian Schallenkamp"
-$boxaccess | Export-Csv -Path C:\Script\Dateien\User_boxaccess.csv -NoTypeInformation -Delimiter ";" -Encoding UTF8
