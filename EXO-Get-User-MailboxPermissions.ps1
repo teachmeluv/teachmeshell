65ab41db5df935e60e26445155d03ac95fcbdaab
Maximilian <m.schallenkamp@gmail.com>
2024-08-29 11:48:06 +0200

    Update EXO-Get-User-MailboxPermissions.ps1

--- EXO-Get-User-MailboxPermissions.ps1
+++ EXO-Get-User-MailboxPermissions.ps1
@@ -1,2 +1,7 @@
-﻿$boxaccess =Get-Mailbox -ResultSize Unlimited | ForEach-Object {Get-MailboxPermission -Identity $_.guid} | Get-MailboxPermission -User "username"
+connect-exchangeonline
+$boxaccess =Get-Mailbox -ResultSize Unlimited | ForEach-Object 
+{
+Get-MailboxPermission -Identity $_.guid
+} 
+| Get-MailboxPermission -User "username"
 $boxaccess | Export-Csv -Path C:\Script\Dateien\User_boxaccess.csv -NoTypeInformation -Delimiter ";" -Encoding UTF8
